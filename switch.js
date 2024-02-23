58ea27bed36c030ee20b2c2f2ffa1a004e3fee46
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-02-22 18:27:06 -0800

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,25 @@
+const day = "Sunday";
+
+switch (day) {
+  case "Monday":
+    console.log("Learn JS");
+    break;
+  case "Tuesday":
+    console.log("Learn DS");
+    break;
+  case "Wednesday":
+    console.log("Learn JS");
+    break;
+  case "Thursday":
+    console.log("Learn Algo");
+    break;
+  case "Friday":
+    console.log("Learn DS");
+    break;
+  case "Saturday":
+  case "Sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day:");
+}
