ce92de84626bb9eef79c7eb20a659d838d418383
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-02-22 18:15:46 -0800

    Add files via upload

---
+++ logicalOperators.js
@@ -0,0 +1,27 @@
+const hasDL = true;
+
+const hasVision = true;
+
+
+console.log(hasDL && hasVision)
+console.log(hasDL || hasVision)
+console.log(!hasDL)
+
+
+if(hasDL && hasVision)
+{
+  console.log("You are able to drive!!")
+}
+else{
+  console.log("Noo")
+}
+
+const boolVal = false
+if(!boolVal)
+{
+  console.log("True");
+}
+else
+{
+  console.log("You are able to drive!!")
+}
