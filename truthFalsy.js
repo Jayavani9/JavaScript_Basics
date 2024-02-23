e4bbf995e12439f77c66cbeb95ed779d3601db04
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-02-22 17:40:51 -0800

    Add files via upload

---
+++ truthFalsy.js
@@ -0,0 +1,21 @@
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("John"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const num = 10;
+
+if (num) {
+  console.log("Its a num");
+} else {
+  console.log("No");
+}
+
+const check = 0;
+
+if (check) {
+  console.log("Its positive");
+} else {
+  console.log("No");
+}
