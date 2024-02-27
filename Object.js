49fea3b4f4c6a32da1fd3a8c46e23b1f17314365
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-02-26 20:33:01 -0800

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,13 @@
+//Objects in JS
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 36,
+  job: "professor",
+  friends: ["michael", "john", "smith"],
+};
+
+console.log(jonas);
+console.log(jonas.firstName);
+console.log(jonas.friends[2]);
